73fc544dafb042362af26f740ea0a0bfa3feffdf
Asad <as9vd@virginia.edu>
2022-08-29 15:16:47 -0400

    Most errors fixed up.

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,15 +1,16 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Scraper {
     public static void main(String[] args) {
+        Map<String, String> footballNames = new HashMap(); // footballer : link
         try {
             String baseLink = "https://en.wikipedia.org/wiki/";
-            FileWriter myWriter = new FileWriter("players.txt");
 
             // Links valid from 1956 - 2009 (2010 is messed up).
             // Sponsored by FIFA from 2010 - 2015.
@@ -26,20 +27,33 @@ public class Scraper {
                 Document doc = Jsoup.connect(baseLink).userAgent("Chrome").get();
 
                 for (Element element: doc.select("table.wikitable").select("tbody")) {
-                    System.out.println(element.select("tr"));
+                    for (Element tr: element.select("tr")) {
+                        // 1. Take care of "[notes 1]".
+                        // 2. Women footballers start showing up 2010, managers as well (latter not a problem if they had a career).
+                        // 3. 2010 is when FIFPro World XI comes up, and the first "td" there is positions (e.g. GK, etc.). So need a new test case for that.
+                        // 4. 2021 has 'Best Club of the Year' table, which happens to be Chelsea (LOL, a club that bought it all).
+                        Elements row = tr.select("tr");
+                        if (!(row.select("td").eq(0).text().isEmpty()) &&
+                                ((int) row.select("td").eq(0).text().charAt(0) > 57) &&
+                                (row.select("td").eq(0).text().length() > 2)
+                        ) { // If this element isn't a empty, a position, or a ranking/number.
+//                            System.out.println(i + ": " + tr.select("td").eq(0).text().split("\\[")[0]);
+                            if (!(tr.select("td").eq(0).select("a[href]").text() == null)) {
+                                footballNames.put(tr.select("td").eq(0).text().split("\\[")[0], tr.select("td").eq(0).attr("href"));
+                            }
+                        } else {
+//                            System.out.println(i + ": " + tr.select("td").eq(1).text().split("\\[")[0]);
+                            if (!(tr.select("td").eq(1).select("a[href").text() == null)) {
+                                footballNames.put(tr.select("td").eq(1).text().split("\\[")[0], tr.select("td").eq(1).attr("href"));
+                            }
+                        }
+                    }
                 }
-
-                myWriter.write(doc.select("table.wikitable")
-                        .select("tbody")
-                        .select("tr")
-                        .select("td")
-                        .get(1)
-                        .text() + "\n");
-
                 baseLink = "https://en.wikipedia.org/wiki/";
             }
 
-            myWriter.close();
+            footballNames.remove(" "); footballNames.remove("");
+            System.out.println(footballNames);
         } catch (Exception e) {
             System.out.println(e.fillInStackTrace());
         }
