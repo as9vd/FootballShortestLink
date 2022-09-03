4034c439c3a233d7ffb51e23cb7da8fc37fcbdf3
Asad <as9vd@virginia.edu>
2022-09-03 19:19:58 -0400

    Home page set-up (no functionality yet however).

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './component/searchbar/searchbar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SearchbarComponent],
+  imports: [BrowserModule, NoopAnimationsModule, MatAutocompleteModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
